0a531886131ae962c97c0fa388f5a34a1f41e5c2
Vandyshge <67765117+Vandyshge@users.noreply.github.com>
2020-12-11 16:47:06 +0300

    на 2 игрока

---
+++ player.py
@@ -0,0 +1,18 @@
+from colors import *
+
+
+class Gamer():
+    def __init__(self, name):
+        self.name = name
+        self.resources = {'gold': 1000, 'building': 0, 'food': 0, 'army': 0}
+        self.myoutpostes_build = []
+        self.myoutpostes = []
+        self.cycle = []
+
+class Computer():
+    def __init__(self):
+        self.name = 'computer'
+        self.resources = {'gold': 1000, 'building': 0, 'food': 0, 'army': 0}
+        self.myoutpostes = []
+        self.myoutpostes_build = []
+        self.cycle = []
