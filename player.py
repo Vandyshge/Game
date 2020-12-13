960d0fc158d49a4cb74b3a0586a408c7f90a708c
KoproKit <71315048+KoproKit@users.noreply.github.com>
2020-12-13 11:02:11 +0300

    Поправила баланс

--- player.py
+++ player.py
@@ -4,7 +4,7 @@ from colors import *
 class Gamer():
     def __init__(self, name):
         self.name = name
-        self.resources = {'gold': 1000, 'building': 0, 'food': 0, 'army': 0}
+        self.resources = {'gold': 1000, 'building': 0, 'food': 0, 'army': 100}
         self.myoutpostes_build = []
         self.myoutpostes = []
         self.cycle = []
@@ -12,7 +12,7 @@ class Gamer():
 class Computer():
     def __init__(self):
         self.name = 'computer'
-        self.resources = {'gold': 1000, 'building': 0, 'food': 0, 'army': 0}
+        self.resources = {'gold': 1000, 'building': 0, 'food': 0, 'army': 100}
         self.myoutpostes = []
         self.myoutpostes_build = []
         self.cycle = []
