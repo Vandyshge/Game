88b8a49971fd49067c0b2876ed58c80a855514fa
Vandyshge <67765117+Vandyshge@users.noreply.github.com>
2020-12-16 20:50:08 +0300

    сделал переходы между страницами, запись топ игроков, добавил енкоторые цвета

--- LI.py
+++ LI.py
@@ -3,11 +3,11 @@ from pygame.draw import *
 from random import randint
 import numpy as np
 
-from objects import *
-from colors import *
 from constants import *
 from menu import *
-from game import *
+from game0 import *
+from game00 import *
+from output import *
 
 
 class Labirint_Fantaxiy():
@@ -16,27 +16,93 @@ class Labirint_Fantaxiy():
         self.page = 'Menu_page'
         pygame.init()
         pygame.display.update()
+        self.name1 = ''
+        self.name2 = ''
+        global pages
+        pages = Pages(self.screen)
         # print('1')
 
     def game(self):
         finished = False
         while not finished:
             if self.page == 'Menu_page':
-                pass
-                menu = Menu(self.screen)
-                out = menu.menu()
+                out = pages.menu()
                 # print('111111')
-                if out == 'Game_page':
-                    self.page = 'Game_page'
+                if out == 'Rule_page':
+                    self.page = 'Rule_page'
                 elif out == True:
                     finished = True
-            elif self.page == 'Game_page':
-                game = Game('georgii', self.screen)
+                elif out == 'Settings_page':
+                    self.page = 'Settings_page'
+                elif out == 'Game_choose':
+                    self.page = 'Game_choose'
+                elif out =='Top_page':
+                    self.page = 'Top_page'
+
+            elif self.page == 'Top_page':
+                out = pages.top()
+                if out == 'Menu_page':
+                    self.page = 'Menu_page'
+                elif out == True:
+                    finished = True
+
+            elif self.page == 'Rule_page':
+                out = pages.rule()
+                if out[0] == 'Game_page':
+                    self.page = 'Game_page'
+                    self.name = out[1]
+                elif out[0] == True:
+                    finished = True
+
+            elif self.page == 'Game_choose':
+                out = pages.game_choose()
+                if out == 'Game0_page':
+                    self.page = 'Game0_page'
+                elif out == True:
+                    finished = True
+                elif out == 'Game00_page':
+                    self.page = 'Game00_page'
+
+            elif self.page == 'Game0_page':
+                out = pages.game0()
+                if out[0] == 'Game0_game_page':
+                    self.page = 'Game0_game_page'
+                    self.name1 = out[1]
+                elif out[0] == True:
+                    finished = True
+
+            elif self.page == 'Game0_game_page':
+                game = Game0(self.name1, self.screen)
                 out = game.game()
                 if out == 'Menu_page':
                     self.page = 'Menu_page'
                 elif out == True:
                     finished = True
+
+            elif self.page == 'Game00_page':
+                out = pages.game00()
+                if out[0] == 'Game00_game_page':
+                    self.page = 'Game00_game_page'
+                    self.name1 = out[1]
+                    self.name2 = out[2]
+                elif out[0] == True:
+                    finished = True
+
+            elif self.page == 'Game00_game_page':
+                game = Game00(self.name1, self.name2, self.screen)
+                out = game.game()
+                if out[0] == 'Menu_page':
+                    self.page = 'Menu_page'
+                    output_top_player('game00', out[1], out[2])
+                elif out[0] == True:
+                    finished = True
+
+            elif self.page == 'Settings_page':
+                out = pages.settings()
+                if out == True:
+                    finished = True
+                elif out == 'Menu_page':
+                    self.page = 'Menu_page'
 
 
 
---
+++ output.py
@@ -0,0 +1,66 @@
+def output_top_player(type_game, win, n_win):
+    if type_game == 'game0':
+        out0 = open('text/top_player_game0.txt', 'r')
+        game0 = []
+        line = out0.readline().strip()
+        line = out0.readline().strip()
+        while line != '':
+            line = line.split(' ')
+            game0.append((int(line[3]), line[1]))
+            line = out0.readline().strip()
+        out0.close()
+
+        out0_w = open('text/top_player_game00.txt', 'w')
+        # добавляем результаты игрока
+        game0.append((n_win, win))
+        # сортируем результаты по счёту игроков
+        game0 = sorted(game0, key=lambda x: -x[0])
+        out0_w.write('Top players in the single player')
+        # записываем рейтинг в тот же файл
+        for i in range(len(game0)):
+            out0_w.write(f'{i + 1}. {game0[i][1]} --- {game0[i][0]}\n')
+        out0_w.close()
+
+    elif type_game == 'game00':
+        out00_r = open('text/top_player_game00.txt', 'r')
+        game00 = []
+        line = out00_r.readline().strip()
+        while line != '':
+            line = line.split(' ')
+            game00.append((int(line[3]), line[1]))
+            line = out00_r.readline().strip()
+        out00_r.close()
+
+        out00_w = open('text/top_player_game00.txt', 'w')
+        # добавляем результаты игрока
+        game00.append((n_win, win))
+        # сортируем результаты по счёту игроков
+        game00 = sorted(game00, key=lambda x: -x[0])
+        out00_w.write('Top players in the game together')
+        # записываем рейтинг в тот же файл
+        for i in range(len(game00)):
+            out00_w.write(f'{i + 1}. {game00[i][1]} --- {game00[i][0]}\n')
+        out00_w.close()
+
+def input_top_player():
+    out0 = open('text/top_player_game0.txt', 'r')
+    game0 = []
+    line = out0.readline().strip()
+    line = out0.readline().strip()
+    while line != '':
+        line = line.split(' ')
+        game0.append((int(line[3]), line[1]))
+        line = out0.readline().strip()
+    out0.close()
+
+    out00_r = open('text/top_player_game00.txt', 'r')
+    game00 = []
+    line = out00_r.readline().strip()
+    line = out00_r.readline().strip()
+    while line != '':
+        line = line.split(' ')
+        game00.append((int(line[3]), line[1]))
+        line = out00_r.readline().strip()
+    out00_r.close()
+
+    return game0, game00
